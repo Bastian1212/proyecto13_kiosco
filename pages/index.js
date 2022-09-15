ea0e5b0d5a3d5bc718d369ef4af4c7560d5451a9
Bastian1212 <bastianvillanueva@me.com>
2022-09-15 13:02:47 -0300

    avances en el layout

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,9 @@
-import { PrismaClient } from '@prisma/client'
-
-export default function Home({categorias}) {
-  console.log(categorias);
+import Layout from "../layout/Layout"
+export default function Home() {
+  
   return (
-
-    <h1>hola mundo</h1>
+    <Layout>
+        <h1>Inicio</h1>
+    </Layout>
   )
-}
-
-export const getServerSideProps = async () => {
-  const prisma = new PrismaClient();
-  const categorias = await prisma.categoria.findMany();
-
-  return{
-    props: { 
-      categorias
-    }
-  }
-
-};
+} 

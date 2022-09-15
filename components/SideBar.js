2bd3fea4f0be4d0ae27514a0feaed4bbb27becd6
Bastian1212 <bastianvillanueva@me.com>
2022-09-15 20:20:27 -0300

    creación del porvider con hooks

---
+++ components/SideBar.js
@@ -0,0 +1,12 @@
+import Image from "next/image"
+function SideBar() {
+    return (
+        <>
+            <Image width={300} height={100}  src={"/assets/img/logo.svg"} alt="imagen logo " />
+            
+        </>
+
+    )
+}
+
+export default SideBar
---
+++ context/KioscoProvider.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect, createContext } from "react";
+
+const KioscoContext = createContext();
+
+
+const KioscoProvider = ({children}) => {
+    return(
+        <KioscoContext.Provider
+            value={{
+
+            }}
+        >
+            {children}
+        </KioscoContext.Provider>
+    )
+}
+
+
+export {
+    KioscoProvider
+}
+
+export default KioscoContext;

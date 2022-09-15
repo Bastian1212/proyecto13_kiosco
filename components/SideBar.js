c2b1f5faf0ea7383b81fc5a90fa1f0f5341869f3
Bastian1212 <bastianvillanueva@me.com>
2022-09-15 20:47:35 -0300

    avances

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,10 +1,26 @@
 import Image from "next/image"
+import useKiosco from "../hooks/useKiosco"
+import Categoria from "./Categoria";
 function SideBar() {
+
+    const {categoria} = useKiosco();
     return (
         <>
-            <Image width={300} height={100}  src={"/assets/img/logo.svg"} alt="imagen logo " />
-            
+            <Image width={300} height={100}  src={"/assets/img/logo.svg"} alt="imagen logo "  />
+            <nav className="mt-10">
+                {categoria.map(categoria => (
+                    <Categoria
+                        key={categoria.key}
+                        categoria={categoria}
+                    />
+
+                ))}
+
+
+            </nav>
         </>
+
+
 
     )
 }
--- context/KioscoProvider.jsx
+++ context/KioscoProvider.jsx
@@ -1,12 +1,20 @@
 import { useState, useEffect, createContext } from "react";
-
+import axios from "axios";
 const KioscoContext = createContext();
 
-
 const KioscoProvider = ({children}) => {
+    const [categoria, setCategoria] = useState([]);
+    const obtenerCategoria = async () => {
+        const {data} = await axios("/api/categorias")
+        setCategoria(data);
+    }
+    useEffect(() => {
+        obtenerCategoria()
+    }, [])
     return(
         <KioscoContext.Provider
             value={{
+                categoria
 
             }}
         >

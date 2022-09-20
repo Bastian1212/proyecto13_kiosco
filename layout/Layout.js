f16c26917fe583bf445d3322f3b60f0d1cf30b16
Bastian1212 <bastianvillanueva@me.com>
2022-09-20 18:35:08 -0300

    creando el modal para el producto

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,7 +1,22 @@
 import Head from "next/head";
 import SideBar from "../components/SideBar";
+import Modal from "react-modal";
+
+import useKiosco from "../hooks/useKiosco";
+const customStyles = {
+    content: {    
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+    },
+};  
+Modal.setAppElement("#__next");
 
 export default function Layout({children, pagina}){
+    const {modal} = useKiosco();
     return(
         <>
             <Head>

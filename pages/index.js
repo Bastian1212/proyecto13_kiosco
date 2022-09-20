85638ac12f3e88fae82b56c8a18987fea92e7b5a
Bastian1212 <bastianvillanueva@me.com>
2022-09-20 14:37:14 -0300

    extrayendo productos mediante  api

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     >
         <h1 className="text-4xl font-black">{categoriaActual?.nombre}</h1>
         <p className="text-2xl my-10">
-          Elige y personaliza tu pedido a continuacion
+          Elige y personaliza tu pedido a continuación
         </p>
     </Layout>
   )

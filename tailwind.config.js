2a2feeb6f0fcda9cd5a0f995018042bd9c6cb17b
Bastian1212 <bastianvillanueva@me.com>
2022-09-20 14:23:26 -0300

    avance

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js.jsx}",
-    "./layout/**/*.{js.jsx}",
-    "./components/**/*.{js.jsx}"
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./layout/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},

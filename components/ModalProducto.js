6c4a26db4313be9774d50e3896dca0e0b4dd22b0
Bastian1212 <bastianvillanueva@me.com>
2022-09-21 17:08:11 -0300

    dando estilos al modal

--- components/ModalProducto.js
+++ components/ModalProducto.js
@@ -36,6 +36,40 @@ function ModalProducto() {
         </div>
         <h1 className='text-3xl font-bold mt-5'>{producto.nombre}</h1>
         <p className='mt-5 font-black text-5xl text-amber-500'>{formatearDinero(producto.precio)}</p>
+        <div className='flex gap-4 mt-5'>
+          <button>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-7 w-7"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+          </button>
+          <button>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-7 w-7"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+          </button>
+        </div>
 
       </div>  
 

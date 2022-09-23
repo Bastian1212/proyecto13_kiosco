8741dde3da0cf0f98aa0fc0b66c30f98db198572
Bastian1212 <bastianvillanueva@me.com>
2022-09-23 18:50:04 -0300

    avaces en total

--- pages/total.js
+++ pages/total.js
@@ -1,11 +1,31 @@
+import { useEffect } from "react"
 import Layout from "../layout/Layout"
+import useKiosco from "../hooks/useKiosco"
 export default function Total () {
+
+    const {pedido} = useKiosco();
+
+    const comprobarPedido  = () => { 
+    
+            return pedido.length === 0
+    }
+    useEffect(() =>{
+        comprobarPedido();
+    }, [pedido, comprobarPedido])
+
+    const enviarOrden = (e) => {
+        e.preventDefault()
+        console.log("hola")
+    }
+
     return (
         <Layout pagina="Resumen ">
             <h1 className="text-4xl font-black">Total y Confirma Pedido</h1>
             <p className="text-2xl my-10">Confime tu Pedido</p>
 
-            <form>
+            <form
+                onSubmit={enviarOrden}
+            >
                 <div>
                     <label className="block uppercase text-slate-800 font-bold">
                         Nombre
@@ -25,8 +45,10 @@ export default function Total () {
                 </div>
                 <div className="mt-5">
                     <input
+                        type="submit"
                         className="bg-indigo-600 w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white text-center"
                         value={"Conformar Pedido "}
+                        disabled={comprobarPedido()}
                     >
 
                     </input>

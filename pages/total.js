f09f9ab9deba7e69ffd5991be073cbd92dedb3f6
Bastian1212 <bastianvillanueva@me.com>
2022-09-23 10:37:29 -0300

    creando las paginas de total y resumen

--- pages/total.js
+++ pages/total.js
@@ -1,6 +1,10 @@
-
+import Layout from "../layout/Layout"
 export default function Total () {
     return (
-        <h1>Datos</h1>
+        <Layout pagina="Resumen ">
+            <h1 className="text-4xl font-black">Total y Confirma Pedido</h1>
+            <p className="text-2xl my-10">Confime tu Pedido</p>
+
+        </Layout>
     )
 }
